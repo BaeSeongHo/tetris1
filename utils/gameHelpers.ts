cf8ab6b8aed4218afc3eca243814fe2b9001bb0e
배성호 <lms4443388@gmail.com>
2026-04-21 18:48:16 +0900

    Fix collision bug where pieces collided with their own ghost shadow

--- utils/gameHelpers.ts
+++ utils/gameHelpers.ts
@@ -20,7 +20,7 @@ export const checkCollision = (
         if (
           !stage[y + player.pos.y + moveY] ||
           !stage[y + player.pos.y + moveY][x + player.pos.x + moveX] ||
-          stage[y + player.pos.y + moveY][x + player.pos.x + moveX][1] !== 'clear'
+          stage[y + player.pos.y + moveY][x + player.pos.x + moveX][1] === 'merged'
         ) {
           return true;
         }
